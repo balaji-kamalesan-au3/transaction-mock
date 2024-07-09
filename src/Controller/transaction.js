b7765e5690675c0553dc784642fc40824fbeb861
Balaji Kamalesan <balaji.kamalesan@ekank.com>
2024-07-09 13:16:18 +0530

    first commit

---
+++ src/Controller/transaction.js
@@ -0,0 +1,14 @@
+const user = {
+  name: "Balaji",
+  amount: 100,
+  transactions: []
+}
+
+const merchant = {
+  name: "Mar",
+  amount: 0,
+  transactions: []
+}
+
+
+const masterTransaction = []
